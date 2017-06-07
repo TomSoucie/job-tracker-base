52d43bba3f18c6552b770035120fa3f234445d2a
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-07 12:43:39 -0600

    Commits final feature test

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,8 @@ class Company < ActiveRecord::Base
   has_many :jobs
   has_many :office_locations
   has_many :offices, through: :office_locations
+
+  def locations(company, location)
+    company.offices.where(name: location)
+  end
 end
